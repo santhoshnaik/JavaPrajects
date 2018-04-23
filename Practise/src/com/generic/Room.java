54390e6dcebc4ad70960388bd4cc28d92e8710eb
santhosh naik nh <santhoshnaik02@gmail.com>
2018-04-23 10:24:06 +0530

    Geniric Examples

---
+++ Practise/src/com/generic/Room.java
@@ -0,0 +1,13 @@
+package com.generic;
+
+public class Room {
+ Object object;
+	public Object add(Object obj)
+	{
+		return this.object=obj;
+	}
+	public Object get()
+	{
+		return object;
+	}
+}
---
+++ Practise/src/com/generic/TestRoom.java
@@ -0,0 +1,14 @@
+package com.generic;
+
+public class TestRoom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Room room=new Room();
+		room.add(60);
+		Integer i=(Integer)room.get();
+		System.out.println(i);
+	}
+
+}
